f9898aab1ed3c87e0f9682ea087c0bb5c9575443
Harsh Rawat <harshrawat66@gmail.com>
2025-01-13 21:10:58 +0530

    FIX: api errors.

--- components/home/FeaturedTrek.tsx
+++ components/home/FeaturedTrek.tsx
@@ -1,26 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Button } from "../ui/button";
 import FeaturedTrekImage from "/public/images/featured-trek.png";
+import { InstagramEmbed } from "react-social-media-embed";
 
-const FeaturedTrek = async () => {
-  const featuredData = await fetch(
-    "https://trekking-mate-services.onrender.com/api/shoutouts"
-  );
+const FeaturedTrek = () => {
+  const [trek, setTrek] = useState({ title: "", description: "", link: "" });
 
-  const data = await featuredData.json();
-  const trek = data?.data?.[0];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://trekking-mate-services.onrender.com/api/shoutouts"
+        );
+        const result = await response.json();
+        setTrek(result?.data?.[0]);
+      } catch (error) {
+        console.error("Error fetching trek data:", error);
+      }
+    };
 
-  console.log(trek);
+    fetchData();
+  }, []);
 
   return (
     <div className="w-full bg-[#121212] h-auto flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-0 my-auto sm:px-36 py-20">
-      <Image
-        src={FeaturedTrekImage}
-        alt="featured-trek"
-        className="w-[90%] sm:w-1/2 object-contain"
-      />
+      <InstagramEmbed url={trek.link} width={330} height={500} />
       <div className="flex flex-col items-center sm:items-start justify-center gap-2 py-4 px-10 sm:w-1/2">
         <p className="text-white font-semibold text-3xl">{trek?.title}</p>
         <p className="text-white/[.6] text-center sm:text-start">
