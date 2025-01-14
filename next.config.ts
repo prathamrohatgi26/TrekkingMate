0142c60935154050021dfef548572d4a2ccb6af8
Harsh Rawat <harshrawat66@gmail.com>
2025-01-14 18:21:35 +0530

    UPDATE: image config.

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "your-bucket-name.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "your-custom-domain.com",
-        pathname: "/uploads/**",
+        hostname: "**",
       },
     ],
   },
